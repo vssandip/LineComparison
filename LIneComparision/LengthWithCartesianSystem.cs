cab82dd01d535d7fbaf212ad6ba2c8903478e408
“Vssandip” <sandipshindesr95035@gmail.com>
2023-01-26 15:38:09 +0530

    [ Sandip ]- Length Of Lines

---
+++ LIneComparision/LengthWithCartesianSystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class LengthWithCartesianSystem
+    {
+        public static void Lines()
+        {
+            double x1, x2, y1, y2;
+
+            Console.WriteLine("Enter the value of x1 : ");
+            x1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of x2 : ");
+             x2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of y1 : ");
+            y1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of y2 : ");
+            y2 = double.Parse(Console.ReadLine());
+
+            double length = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+
+            Console.WriteLine("The length between the two point is: "  +  length);
+        }
+    }
+}
---
+++ LIneComparision/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LIneComparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
